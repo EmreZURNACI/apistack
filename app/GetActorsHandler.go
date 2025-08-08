1c1af48827c10c8fb4f6452155cc4def091c187e
Emre ZURNACI <emrezurnaci38@gmail.com>
2025-08-08 20:49:19 +0300

    feat(actor): add handler to get all actors informations

---
+++ app/GetActorsHandler.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"context"
+	"github.com/EmreZURNACI/apistack.git/domain"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type GetActorsRequest struct {
+	Search  string `json:"search"`
+	Limit   int    `json:"limit"`
+	Offset  int    `json:"offset"`
+	OrderBy bool   `json:"order_by"`
+}
+type GetActorsResponse struct {
+	Actors []domain.Actor `json:"actors"`
+}
+
+type GetActorsHandler struct {
+	Repository Repository
+	Tracer     trace.Tracer
+}
+
+func NewGetActorsHandler(repository Repository, tracer trace.Tracer) *GetActorsHandler {
+	return &GetActorsHandler{
+		Repository: repository,
+		Tracer:     tracer,
+	}
+}
+
+func (h *GetActorsHandler) Handle(ctx context.Context, req *GetActorsRequest) (*GetActorsResponse, error) {
+
+	ctx, span := h.Tracer.Start(ctx, "GetActorsHandle")
+	defer span.End()
+	
+	actors, err := h.Repository.GetActors(ctx, req.Search, req.Offset, req.Limit, req.OrderBy)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetActorsResponse{
+		Actors: actors,
+	}, nil
+}
