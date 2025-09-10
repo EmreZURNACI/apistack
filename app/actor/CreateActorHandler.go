fe6183742f74a287a1ff05bcac29d968ff1c5805
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2025-09-10 20:37:26 +0300

    refactor: Update imports and move files under actor directory

---
+++ app/actor/CreateActorHandler.go
@@ -0,0 +1,42 @@
+package actor
+
+import (
+	"context"
+
+	"github.com/EmreZURNACI/apistack/app"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type CreateActorRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type CreateActorResponse struct {
+	ID int64 `json:"id"`
+}
+type CreateActorHandler struct {
+	Repository app.Repository
+	Tracer     trace.Tracer
+}
+
+func NewCreateActorHandler(repository app.Repository, trace trace.Tracer) *CreateActorHandler {
+	return &CreateActorHandler{
+		Repository: repository,
+		Tracer:     trace,
+	}
+}
+
+func (h *CreateActorHandler) Handle(ctx context.Context, req *CreateActorRequest) (*CreateActorResponse, error) {
+
+	ctx, span := h.Tracer.Start(ctx, "CreateActorHandle")
+	defer span.End()
+
+	id, err := h.Repository.CreateActor(ctx, req.FirstName, req.LastName)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateActorResponse{
+		ID: id,
+	}, nil
+}
---
+++ app/actor/DeleteActorHandler.go
@@ -0,0 +1,42 @@
+package actor
+
+import (
+	"context"
+
+	"github.com/EmreZURNACI/apistack/app"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type DeleteActorRequest struct {
+	ID string `json:"id"`
+}
+type DeleteActorResponse struct {
+	Message string `json:"message"`
+}
+
+type DeleteActorHandler struct {
+	Repository app.Repository
+	Tracer     trace.Tracer
+}
+
+func NewDeleteActorHandler(repository app.Repository, trace trace.Tracer) *DeleteActorHandler {
+	return &DeleteActorHandler{
+		Repository: repository,
+		Tracer:     trace,
+	}
+}
+
+func (h *DeleteActorHandler) Handle(ctx context.Context, req *DeleteActorRequest) (*DeleteActorResponse, error) {
+
+	ctx, span := h.Tracer.Start(ctx, "DeleteActorHandle")
+	defer span.End()
+
+	err := h.Repository.DeleteActor(ctx, req.ID)
+	if err != nil {
+		return &DeleteActorResponse{}, err
+	}
+	return &DeleteActorResponse{
+		Message: "Aktör silindi",
+	}, nil
+
+}
