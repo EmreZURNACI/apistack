7199d188ce64b57f58fc6cc6055fae210566ed1f
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2025-09-24 19:46:53 +0300

    feat : add gorm struct tags for create table instance

--- domain/actor.go
+++ domain/actor.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type Actor struct {
-	ActorID    int64     `json:"actor_id"`
-	FirstName  string    `json:"first_name"`
-	LastName   string    `json:"last_name"`
-	LastUpdate time.Time `json:"last_update"`
+	ID         int64     `json:"ID" gorm:"primaryKey;type:SERIAL;"`
+	FirstName  string    `json:"FirstName" gorm:"type:VARCHAR(100);NOT NULL;"`
+	LastName   string    `json:"LastName" gorm:"type:VARCHAR(100);NOT NULL;"`
+	LastUpdate time.Time `json:"LastUpdate" gorm:"default:CURRENT_TIMESTAMP;NOT NULL;"`
 }
